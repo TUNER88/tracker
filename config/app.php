04ab83033292b8d1d3c79079504b18cb377d1e3f
TUNER88 <anton.pauli@gmail.com>
2015-12-02 00:06:28 +0100

    add package development tools

--- config/app.php
+++ config/app.php
@@ -145,6 +145,10 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Third party Service Providers...
+         */
+        JeroenG\Packager\PackagerServiceProvider::class,
     ],
 
     /*
